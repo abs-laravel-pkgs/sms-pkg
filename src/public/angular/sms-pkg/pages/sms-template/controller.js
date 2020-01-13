896b9cd72a873b2faa7625198022a1a813c54c36
Abdul <abdul@uitoux.in>
2020-01-13 11:57:09 +0530

    u

--- src/public/angular/sms-pkg/pages/sms-template/controller.js
+++ src/public/angular/sms-pkg/pages/sms-template/controller.js
@@ -1,3 +1,21 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.
+    //CUSTOMER
+    when('/sms-pkg/sms/list', {
+        template: '<sms-list></sms-list>',
+        title: 'Smss',
+    }).
+    when('/sms-pkg/sms/add', {
+        template: '<sms-form></sms-form>',
+        title: 'Add Sms',
+    }).
+    when('/sms-pkg/sms/edit/:id', {
+        template: '<sms-form></sms-form>',
+        title: 'Edit Sms',
+    });
+}]);
+
 app.component('smsList', {
     templateUrl: sms_list_template_url,
     controller: function($http, $location, HelperService, $scope, $routeParams, $rootScope, $location) {
